0ff55632c9a76bfed4b92be22d53f836c808b9e2
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-04-18 21:41:33 +0300

    Create the WeatherState and WeatherPublisher classes

---
+++ Models/WeatherPublisher.cs
@@ -0,0 +1,33 @@
+﻿using WeatherMonitor.Publisher;
+
+namespace WeatherMonitor.Models;
+
+public class WeatherPublisher : IPublisher
+{
+    private readonly List<ISubscriber> _subscribers = new List<ISubscriber>();
+    private WeatherState _weatherState = new WeatherState();
+
+    public void Attach(ISubscriber subscriber)
+    {
+        _subscribers.Add(subscriber);
+    }
+
+    public void Detach(ISubscriber subscriber)
+    {
+        _subscribers.Remove(subscriber);
+    }
+
+    public void Notify()
+    {
+        foreach (var subscriber in _subscribers)
+        {
+            subscriber.Update(this);
+        }
+    }
+
+    public void ChangeWeatherState(WeatherState newState)
+    {
+        _weatherState = newState;
+        Notify();
+    }
+}
---
+++ Models/WeatherState.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherMonitor.Models;
+
+public class WeatherState
+{
+    public string Location { get; set; } = string.Empty;
+    public double Temperature { get; set; }
+    public double Humidity { get; set; }
+}
